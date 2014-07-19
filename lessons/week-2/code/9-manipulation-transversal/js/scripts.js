4cd23c35fe409c0dcb0db59e8ad4804bed776e7c
Kenton Hansen <kenton@gobananago.com>
2014-07-18 23:54:40 -0500

    Add week 2 lesson 7 and 8 resources

---
+++ lessons/week-2/code/9-manipulation-transversal/js/scripts.js
@@ -0,0 +1,24 @@
+$("button#hello").click(function() {
+  $("ul#user").prepend("<li>Hello! <span class='clickable delete'>x</span></li>");
+  $("ul#webpage").prepend("<li>Why hello there! <span class='clickable delete'>x</span></li>");
+  $('li').click(function() {
+    alert('hi');
+  });
+});
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+  });
+});
+$("ul#user").children("li").first().click(function() {
+  $(this).remove();
+});
+$("ul#webpage").children("li").first().click(function() {
+  $(this).remove();
+});
