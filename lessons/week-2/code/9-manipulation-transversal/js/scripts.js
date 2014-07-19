3778ace26693738750525d0406348e5f5362160c
kentonh <kenton@gobananago.com>
2014-07-19 00:27:02 -0500

    Week 2 lesson 9 code

--- lessons/week-2/code/9-manipulation-transversal/js/scripts.js
+++ lessons/week-2/code/9-manipulation-transversal/js/scripts.js
@@ -1,10 +1,15 @@
-$("button#hello").click(function() {
-  $("ul#user").prepend("<li>Hello! <span class='clickable delete'>x</span></li>");
-  $("ul#webpage").prepend("<li>Why hello there! <span class='clickable delete'>x</span></li>");
-  $('li').click(function() {
-    alert('hi');
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
-});
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
@@ -15,10 +20,4 @@ $("button#hello").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
   });
-});
-$("ul#user").children("li").first().click(function() {
-  $(this).remove();
-});
-$("ul#webpage").children("li").first().click(function() {
-  $(this).remove();
 });
