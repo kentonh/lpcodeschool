77e600336881a6a0d9e8d5a098ba918ce28a2fef
Jacob Walker <jwalker@timeips.com>
2014-07-29 22:58:38 -0500

    Add skeleton app with Mocha and Chai BDD tools in place

---
+++ lessons/week-3/code/bdd-skeleton/spec/specs.js
@@ -0,0 +1,9 @@
+describe('example tests', function() {
+    it('should handle the obvious', function() {
+        false.should.equal(false);
+    });
+
+    it('can\'t handle the truth', function() {
+        true.should.equal(false);
+    });
+});
